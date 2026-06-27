55e4a9a3cac5df8d4ee33622bd4006f07d69ef0c
Admin <romanpluta42@gmail.com>
2026-06-27 11:42:14 +0300

    fix 1

--- src/isIsogram.test.js
+++ src/isIsogram.test.js
@@ -7,23 +7,37 @@ describe('isIsogram', () => {
     expect(isIsogram).toBeInstanceOf(Function);
   });
 
-  it('should return true for a word with no repeating letters ("playgrounds")', () => {
-    expect(isIsogram('playgrounds')).toBe(true);
-  });
+  it(
+    'should return true for a word with no repeating letters ("playgrounds")',
+    () => {
+      expect(isIsogram('playgrounds')).toBe(true);
+    }
+  );
 
-  it('should return false for a word with consecutive repeating letters ("look")', () => {
-    expect(isIsogram('look')).toBe(false);
-  });
+  it(
+    'should return false for a word with consecutive repeating letters ("look")',
+    () => {
+      expect(isIsogram('look')).toBe(false);
+    }
+  );
 
-  it('should return false for a word with non-consecutive repeating letters of different cases ("Adam")', () => {
-    expect(isIsogram('Adam')).toBe(false);
-  });
+  it(
+    'should return false for a word with non-consecutive repeating letters ' +
+    'of different cases ("Adam")',
+    () => {
+      expect(isIsogram('Adam')).toBe(false);
+    }
+  );
 
   it('should return true for an empty string', () => {
     expect(isIsogram('')).toBe(true);
   });
 
-  it('should return false for a word starting with repeating letters of different cases ("Oops")', () => {
-    expect(isIsogram('Oops')).toBe(false);
-  });
+  it(
+    'should return false for a word starting with repeating letters ' +
+    'of different cases ("Oops")',
+    () => {
+      expect(isIsogram('Oops')).toBe(false);
+    }
+  );
 });
